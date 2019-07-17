f703b8ee9a3b449be7ea84478f04003bd3790891
Strahinja Kovacevic <strahinja.kovacevic@sbgenomics.com>
2019-07-17 18:01:24 +0200

    Removed Layout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import React  from 'react';
+import {connect} from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import {withStyles} from "@material-ui/core";
+import LandingLayout from "./components/Layout/LandingLayout/LandingLayout";
+import MainLayout from "./components/Layout/MainLayout/MainLayout";
 
-class App extends Component {
-
-    render() {
-        return (
-            <Layout />
-        );
+const styles = theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        }
+    },
+    container: {
+        display: 'flex'
     }
-}
+});
 
-export default App;
+const app = (props) => {
+    const layout = props.authenticated ? <MainLayout/> : <LandingLayout/>;
+    return (
+        <Container component="main" maxWidth="xs" className={props.classes.container} >
+            <CssBaseline />
+            {layout}
+        </Container>
+    )
+};
+
+const mapStateToProps = state => {
+    return {
+        authenticated: state.authenticated
+    }
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(app));
